a94f68775e227018a8ceb32a302d849174a865e8
joekennerly <joseph.kennerly@gmail.com>
2019-10-19 16:40:06 -0500

    babelrc + webpack.config + index.html

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class App extends Component {
+    render() {
+        return <h1>React App</h1>
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
